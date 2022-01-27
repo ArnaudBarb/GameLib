e2822f7fc555418216c2bbaaa002e9d3c1d45b05
Arnaud Barberon <barberon_a@hotmail.fr>
2022-01-27 15:37:18 +0100

    fichier nav.php

--- includes/main.php
+++ includes/main.php
@@ -1,3 +1,5 @@
 <h1>GameLib</h1>
 
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, saepe? Officia consequatur quos repudiandae, placeat similique praesentium nemo debitis harum.</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, saepe? Officia 
+  consequatur quos repudiandae, placeat similique praesentium nemo debitis harum.</p>
+  
---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.php?page=home">Home</a></li>
+            <li><a href="index.php?page=news">News</a></li>
+            <li><a href="index.php?page=contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
