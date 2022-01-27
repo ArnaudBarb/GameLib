b7db3d8b2a72d6e5f8689f037797177fe12022d9
Arnaud Barberon <barberon_a@hotmail.fr>
2022-01-27 15:39:03 +0100

    inclusion fichier nav.php

--- includes/main.php
+++ includes/main.php
@@ -1,5 +1,8 @@
+<?php
+require_once'./includes/nav.php'
+?>
+
 <h1>GameLib</h1>
 
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore, saepe? Officia 
   consequatur quos repudiandae, placeat similique praesentium nemo debitis harum.</p>
-  
