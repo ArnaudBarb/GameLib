938fecca1c228f51bbc69e9f8fdf63497fd6cbb0
Arnaud Barberon <barberon_a@hotmail.fr>
2022-01-28 14:22:55 +0100

    verif formulaire plus mdp

--- includes/formulaire.inc.php
+++ includes/formulaire.inc.php
@@ -1,9 +1,67 @@
 <?php
-
+//traitement pour le remplissage du fomulaire
 if (isset($_POST['frm'])) {
-  echo "Formulaire déjà rempli, recommencer?";
+  //Appel des variables pour remplir le fomrulaire; trim pour enlever les espaces dans le formulaire
+  $nom = trim($_POST['nom']) ?? '';
+  $prenom = trim($_POST['prenom']) ?? '';
+  $email = trim($_POST['email']) ?? '';
+  $motDePasse = $_POST['motDePasse'] ?? '';
+  $confirmerMotDePasse = $_POST['confirmerMotDePasse'] ?? '';
+
+//création d'un tableau vide afin de récupérer les données du formulaire, sert à traiter les occurences vides
+  $erreur = array();
+//Condition pour vérifier le remplissage des champs; insertion d'un message d'erreur pour les champs vides
+    if(strlen($nom) === 0)
+      array_push($erreur, "Veuillez saisir votre nom");
+
+    elseif(!ctype_alpha($nom))
+      array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if(strlen($prenom) === 0)
+      array_push($erreur, "Veuillez saisir votre prenom");
+
+    elseif(!ctype_alpha($prenom))
+      array_push($erreur, "Veuillez saisir des caractères alphabétiques");
+
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+      array_push($erreur, "Veuillez saisir un e-mail valide");
+
+    if(strlen($motDePasse) === 0)
+      array_push($erreur, "Veuillez saisir un mot de passe");
+
+    if(strlen($confirmerMotDePasse) === 0)
+      array_push($erreur, "Veuillez confirmer votre mot de passe");
+
+    elseif($confirmerMotDePasse !== $motDePasse)
+      array_push($erreur, "Confirmation inexacte, veuillez recommencer svp");
+  
+    if(count($erreur) === 0){
+      echo "traitement du formulaire";
+    }
+//else= affichage du message d'erreur sous forme de liste dans le cas d'un champ vide
+    else{
+      //création d'une variable rendant son contenu sous forme de liste
+      $messageErreur = "<ul>";
+      //création d'une variable pour cibler une erreur précise ("index tableau")
+      $i = 0;
+      do {
+        $messageErreur .= "<li>";
+        $messageErreur .= $erreur[$i];
+        $messageErreur .= "</li>";
+        $i++;
+      }
+
+      while($i < count($erreur));
+
+      $messageErreur .="</ul>";
+      echo $messageErreur;
+
+      echo password_hash($motDePasse, PASSWORD_BCRYPT);
+    }
+
 } else{
   echo "Merci de remplir le formulaire";
+  $nom = $prenom = $email = '';
 }
 
 include 'frmFormulaire.php';
--- includes/frmFormulaire.php
+++ includes/frmFormulaire.php
@@ -1,10 +1,19 @@
 <form action="index.php?page=formulaire" method="post">
     <label for="nom">Nom :</label>
-    <input type="text" name="nom" id="nom" /> <br />
+    <input type="text" name="nom" id="nom" value="<?php echo $nom;?>" /> <br />
+
     <label for="prenom">Prenom :</label>
-    <input type="text" name="prenom" id="prenom" /><br />
+    <input type="text" name="prenom" id="prenom" value="<?php echo $prenom;?>" /><br />
+
     <label for="email">e-mail :</label>
-    <input type="text" name="email" id="email" /><br />
+    <input type="text" name="email" id="email" value="<?php echo $email;?>" /><br />
+
+    <label for="password">Mot de Passe</label>
+    <input type="password" name="motDePasse" id="motDePasse" /><br />
+
+    <label for="password">Confirmer mot de Passe :</label>
+    <input type="password" name="confirmerMotDePasse" id="confirmerMotDePasse" /><br />
+
     <input type="reset" value="Effacer"> 
     <input type="submit" value="Valider le formulaire">
     <input type="hidden" name="frm" />
