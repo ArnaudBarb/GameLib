0ab74139344e6d15c3b5a701bf0f84e81725fd8f
Arnaud Barberon <barberon_a@hotmail.fr>
2022-02-02 13:32:26 +0100

    indentation

--- classes/sql.php
+++ classes/sql.php
@@ -2,38 +2,38 @@
 
 class Sql
 {
-    private string $serverName = "localhost";
-    private string $userName = "root";
-    private string $database = "exercice";
-    private string $userPassword = "";
-    private object $connexion;
+  private string $serverName = "localhost";
+  private string $userName = "root";
+  private string $database = "exercice";
+  private string $userPassword = "";
+  private object $connexion;
 
-    public function __construct()
-    {
-        try{
-            $this->connexion = new PDO("mysql:host=$this->serverName;dbname=$this->database",
-            $this->userName, $this->userPassword);
-        }
-        catch(PDOException $e){
-            die("Erreur : " .$e->getMessage());
-        }
+  public function __construct()
+  {
+    try{
+        $this->connexion = new PDO("mysql:host=$this->serverName;dbname=$this->database",
+        $this->userName, $this->userPassword);
     }
+    catch(PDOException $e){
+        die("Erreur : " .$e->getMessage());
+    }
+  }
 
-    public function insertion($sql)
-    {
-        try{
-            $this->connexion->beginTransaction();
-            $this->connexion->exec($sql);
-            $this->connexion->commit();
-        }
-        catch(PDOException $e){
-            $this->connexion->rollBack();
-            die("Erreur : " .$e->getMessage());
-        }
+  public function insertion($sql)
+  {
+    try{
+      $this->connexion->beginTransaction();
+      $this->connexion->exec($sql);
+      $this->connexion->commit();
     }
+    catch(PDOException $e){
+      $this->connexion->rollBack();
+      die("Erreur : " .$e->getMessage());
+    }
+  }
 
-    public function __destruct()
-    {
-        unset($this->connexion);
-    }
+  public function __destruct()
+  {
+    unset($this->connexion);
+  }
 }
